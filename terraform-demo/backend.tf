8936284a6cfbeadc0f98e6f8c6082f7913b23a8e
Curtis Rubeck <rubeckcurtis@gmail.com>
2024-09-05 22:07:00 -0500

    remove duplicate backend

--- terraform-demo/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-992382678496"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock-992382678496"
-  }
-}
